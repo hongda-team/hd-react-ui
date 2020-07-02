ce665e80611eb24879548d72a31f2c6520e4ce55
zhanghongda <zhanghongda@atwasoft.net>
2020-07-02 14:43:31 +0800

    button按钮基本功能完善

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ class App extends Component {
   render() {
     return (
       <div className='app'>
-        <Button text={'点我'}></Button>
+        <Button type='primary' size='large' handleClick={() => console.log(111)}>Primary Button</Button>
+        <Button size='small' disabled handleClick={() => console.log(222)}>Small Button</Button>
+        <Button type='dashed' size='middle'>Dashed Button</Button>
+        <Button type='text' size='large'>Text Button</Button>
+        <Button type='link' size='small'>Link Button</Button>
+        <Button>默认按钮</Button>
       </div>
     )
   }
--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import classnames from 'classnames'
 import './index.css'
 
-const Button = ({ text }) => <button className='button'>Button组件{text}</button>
+class Button extends Component {
+  render() {
+    const {
+      children,
+      type,
+      size,
+      disabled,
+      handleClick
+    } = this.props
+    const className = classnames({
+      'hd-btn': true,
+      'hd-btn-large': size === 'large',
+      'hd-btn-small': size === 'small',
+      'hd-btn-primary': type === 'primary',
+      'hd-btn-dashed': type === 'dashed',
+      'hd-btn-text': type === 'text',
+      'hd-btn-link': type === 'link'
+    })
 
-Button.propTypes = {
-  text: PropTypes.any
+    return (
+      <button className={className} onClick={handleClick} disabled={disabled}>
+        {children}
+      </button>
+    )
+  }
+}
+
+Button.defauleProps = {
+  children: 'Button',
+  type: 'primary',
+  size: 'middle',
+  disabled: false,
+  handleClick: () => { return null }
 }
 
 export default Button
